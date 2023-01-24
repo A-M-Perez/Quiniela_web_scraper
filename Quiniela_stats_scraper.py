3cfbbd8662fd2e5b1f3717553a59b2b443f3b148
Martin Perez <aldomartinperez@hotmail.com>
2023-01-24 20:23:17 -0300

    Completed scraper file with comments. Updated README file.

--- Quiniela_stats_scraper.py
+++ Quiniela_stats_scraper.py
@@ -10,8 +10,8 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-# Define arrays of available "Quinielas" and "Periods" to be scraped (all available options included below)
-quinielas_list = ['nacional', 'buenos-aires', 'misiones', 'corrientes', 'san-luis', 'mendoza', 'san-juan', 'salta', 'chaco', 'uruguay', 'formosa', 'santa-fe', 'cordoba', 'entre-rios', 'tucuman', 'santiago-del-estero', 'neuquen']
+# Create arrays of available "Quinielas" and "Periods" to be scraped (all available options included below)
+quinielas_list = ['nacional', 'buenos-aires', 'misiones', 'corrientes', 'san-luis', 'mendoza', 'tucuman', 'santiago-del-estero', 'santa-fe', 'cordoba', 'entre-rios', 'neuquen']
 periods_list = ['primera', 'matutina', 'vespertina', 'nocturna']
 
 # Iterate through available "Quinielas", and for each of them, through all available "Periods"
@@ -22,9 +22,9 @@ for quiniela_argument in quinielas_list:
         # Define a range of dates for scraping data within that timeframe (largest date range defined below)
         results_date = date(2014, 10, 31) # Oldest date with data in the website is 2014-11-01 
         end_date = date.today()
-        date_delta = timedelta(days=1)
+        date_delta = timedelta(days=1) # Steps of 1 day in the iteration
 
-        # Create and set the structure of the file to be exported with scraped data (one file per Quiniela & Period)
+        # Create and set the structure of the CSV file to be exported with scraped data (one file per Quiniela & Period)
         file = open('./data/{}-{}.csv'.format(quiniela_argument, period_argument), 'w', newline='')
         writer = csv.writer(file)
         quiniela_file_headers = ['date', 'quiniela', 'period', 'position', 'result']
@@ -41,16 +41,21 @@ for quiniela_argument in quinielas_list:
 
             # Create array to store individual table rows
             quiniela_file_rows = []
-            # Find rows with lottery results
-            rows = soup.find_all('td', class_='celNor tacenter')
             
-            # Iterate through the lottery results table rows and cells, writing them individually into the CSV file
+            # Since some days may not have information, to avoid timeout, do "try... except..."
             try:
-                i = 0
-                while i < 42: # There are 21 rows with 2 relevant cells per row ("position" & "result")
-                    quiniela_file_rows = [results_date.strftime('%Y-%m-%d'), quiniela_argument, period_argument, rows[i].text, rows[i+1].text]
-                    i += 2
-                    writer.writerow(quiniela_file_rows)
+                # Find rows with lottery results
+                rows = soup.find_all('td', class_='celNor tacenter')
+                
+                # Iterate through the lottery results table rows and cells, writing them individually into the CSV file
+                try:
+                    i = 0
+                    while i < 42: # There are 21 rows with 2 relevant cells per row ("position" & "result")
+                        quiniela_file_rows = [results_date.strftime('%Y-%m-%d'), quiniela_argument, period_argument, rows[i].text, rows[i+1].text]
+                        i += 2
+                        writer.writerow(quiniela_file_rows)
+                except:
+                    continue
             except:
                 continue
 
@@ -58,6 +63,6 @@ for quiniela_argument in quinielas_list:
 
 exit()
 
-# #UNCOMMENT CODE TO CHECK OUT PAGE HTML
+# #UNCOMMENT CODE TO EXPORT AND CHECK OUT PAGE HTML
 # with open('lottery_numbers_page_template.html', 'wb') as file:
 #     file.write(soup.prettify('utf-8'))
--- README.md
+++ README.md
@@ -12,35 +12,47 @@ This project aims to build a web scraper, using Python with BeautifulSoup, to fe
 - BeautifulSoup
 - ssl
 - csv
-- re
-- time
+- date / timedelta
 
-## Data dictionary
+## Data dictionary (scraped data)
 
-- ***xx***: xxx
-- ***xx***: xxx
+- ***date***: This field contains the date in which the lottery took place. Values can be duplicated, each for a different Quiniela, Period, and Result position. Values should of type DATE.
 
+- ***quiniela***: This field contains the name of the "Quiniela" (lottery), usually referring to the province in which it took place, or "nacional", meaning nation wide. Values can be duplicated, since the same lottery encompasses different dates, Periods, and results. Values are of type TEXT. See possible values in the scraper file's comments.
 
-Quiniela: https://www.nacionalloteria.com/argentina/quinielas.php
+- ***period***: This field contains the name of the period (time of the day) in which the lottery took place. Values can be duplicated since periods repeat in different dates. Possible values are "primera" (earliest in the morning), "matutina" (morning), "vespertina" (afternoon), "nocturna" (night). Values are of type TEXT.
 
-number_interpretation_list: https://www.lanacion.com.ar/loterias/significado-de-suenos-y-numeros-para-loterias-y-quinielas-nid09042022/
+- ***position***: This field contains the position number in which the result was drawn. There are 20 -4 digit- numbers drawn in each Quiniela, Period and date. Values can be duplicated since every Quiniela and Period have the same number of positions. Values are of type NUMBER.
 
+- ***result***: This field contains the result for the specific lottery and position. Values cannot be duplicated for the same Quiniela, Period and date. Values are of type NUMBER and should integers of 4 digits.
+
+> Source : https://www.nacionalloteria.com/argentina/quinielas.php
 
 
 ## Repository overview / structure
 
 ├── README.md\
-├── Quiniela_stats_scraper.ipynb (scrapes Quiniela's historical data)\
-├── x
+├── Quiniela_stats_scraper.py (scrapes Quiniela's historical data)\
+├── data (scraped data results)\
+&emsp;&emsp;├── buenos-aires-primera.csv\
+&emsp;&emsp;├── buenos-aires-matutina.csv\
+&emsp;&emsp;├── buenos-aires-vespertina.csv\
+&emsp;&emsp;├── buenos-aires-nocturna.csv\
+&emsp;&emsp;├── nacional-primera.csv\
+&emsp;&emsp;├── nacional-matutina.csv\
+&emsp;&emsp;├── nacional-vespertina.csv\
+&emsp;&emsp;├── nacional-nocturna.csv
+
 
 ## Running instructions
 
->*DO NOT FORGET to check the xxxx file to ensure compliance with the website's search engine crawlers policy*
+>*DO NOT FORGET to check the https://www.nacionalloteria.com/robots.txt file to ensure compliance with the website's search engine crawlers policy*
 
 To test or run this script, please:
 
-- Clone the repository or download the file ""
-- Import the file "" to your favourite integrated development environment (IDE) supporting Python
+- Clone the repository or download the file "Quiniela_stats_scraper.py"
+- Import the file "Quiniela_stats_scraper.py" to your favourite integrated development environment (IDE) supporting Python
+- Edit the arrays for "Quinielas" and "Periods" to fit your objectives of scraped data
 - Run the entire script
 
 ## How this project helped me grow:
